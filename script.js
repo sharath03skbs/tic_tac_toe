2f8f8614f2fe1f67a6fa417a97e79b68bd1c7b3d
Sharath Kumar <sharathskbs111@gmail.com>
2024-02-18 22:51:57 +0530

    Updated script

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let msgContainer = document.querySelector('.msg-container');
 let msg = document.querySelector('#msg');
 
 let cnt = 0;
+//Using cnt to implement draw condition
 
 let turnO = true; //playerO 's turn 
 
@@ -34,7 +35,7 @@ const resetGame =()=>{
 
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
-        if (cnt < 8){
+        cnt++;
         if (turnO){
             box.innerText = "O";
             turnO = false;
@@ -46,23 +47,18 @@ boxes.forEach((box)=>{
             box.style.color = "navy"
         }
         box.disabled = true;
-        cnt ++;
-
+        //console.log(cnt);
         checkWinner();
-    }else {
-        msg.innerText = "Draw Match !. No moves  left!.";
-        msgContainer.classList.remove("hide");
-        disableBoxes();
-        mainGame.classList.add("hide");
-    }
     })
 });
 
 const showWinner =(winner)=>{
+    
     msg.innerText = "Congratulations !! "+ winner +" is the Winner! ";
     msgContainer.classList.remove("hide");
     disableBoxes();
     mainGame.classList.add("hide");
+    
 };
 
 const disableBoxes = ()=>{
@@ -86,20 +82,28 @@ const strikeBoxes =(box1,box2,box3)=>{
 };
 
 const checkWinner =()=>{
-for(pattern of winningpatterns){
-    //These 3 positions will be compared to all the patterns
-    let pos1val = boxes[pattern[0]].innerText;  //This will have the value of the first element of the pattern
-    let pos2val = boxes[pattern[1]].innerText;
-    let pos3val = boxes[pattern[2]].innerText;
+ 
+    for(pattern of winningpatterns){
+        //These 3 positions will be compared to all the patterns
+        let pos1val = boxes[pattern[0]].innerText;  //This will have the value of the first element of the pattern
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
 
-    if(pos1val != "" && pos2val != "" && pos3val != "" ) // checks if neither of the boxes are empty
-    {
-        if(pos1val == pos2val && pos2val == pos3val){
-            strikeBoxes(pattern[0],pattern[1],pattern[2]);
-            showWinner(pos1val);
+        if(pos1val != "" && pos2val != "" && pos3val != "" ) // checks if neither of the boxes are empty
+        {
+            if(pos1val == pos2val && pos2val == pos3val){
+                strikeBoxes(pattern[0],pattern[1],pattern[2]);
+                showWinner(pos1val);
+                return;
+            }
         }
     }
-}
+    if(cnt === 9){
+            msg.innerText = "Draw ! No moves left !! ";
+            msgContainer.classList.remove("hide");
+            disableBoxes();
+            mainGame.classList.add("hide");   
+    } 
 };
 
 resetBtn.addEventListener( 'click', resetGame);
